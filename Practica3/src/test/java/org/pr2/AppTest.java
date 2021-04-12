d238d61a9deb0e0dc7c118d788c0757dd477918f
daguerre45 <eldeprueba45@gmail.com>
2021-04-12 11:15:59 +0200

    Copy base de la practica

--- Practica3/src/test/java/org/pr2/AppTest.java
+++ Practica3/src/test/java/org/pr2/AppTest.java
@@ -1,20 +1,38 @@
 package org.pr2;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
-     * Rigorous Test :-)
+     * Este test comprueba que el método ‘onePath(V v1, V v2)‘ encuentra un camino
+     * entre ‘v1‘ y ‘v2‘ cuando existe.
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void onePathFindsAPath() {
+        System.out.println("\nTest onePathFindsAPath");
+        System.out.println("----------------------");
+        // Se construye el grafo.
+        Graph<Integer> g = new Graph<>();
+        g.addEdge(1, 2);
+        g.addEdge(3, 4);
+        g.addEdge(1, 5);
+        g.addEdge(5, 6);
+        g.addEdge(6, 4);
+        // Se construye el camino esperado.
+        List<Integer> expectedPath = new ArrayList<>();
+        expectedPath.add(1);
+        expectedPath.add(5);
+        expectedPath.add(6);
+        expectedPath.add(4);
+        // Se comprueba si el camino devuelto es igual al esperado.
+        assertEquals(expectedPath, g.onePath(1, 4));
     }
 }
